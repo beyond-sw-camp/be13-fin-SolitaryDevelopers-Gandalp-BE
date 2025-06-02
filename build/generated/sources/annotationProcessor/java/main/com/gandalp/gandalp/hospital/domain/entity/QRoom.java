755918890cee83f92aeb45a724a13da1b5f0963b
gong4857 <village1031@gmail.com>
2025-06-02 11:34:02 +0900

    [경태] 배포 URL 설정중

--- build/generated/sources/annotationProcessor/java/main/com/gandalp/gandalp/hospital/domain/entity/QRoom.java
+++ build/generated/sources/annotationProcessor/java/main/com/gandalp/gandalp/hospital/domain/entity/QRoom.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,6 +18,8 @@ public class QRoom extends EntityPathBase<Room> {
 
     private static final long serialVersionUID = 2073464977L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QRoom room = new QRoom("room");
 
     public final com.gandalp.gandalp.common.entity.QBaseEntity _super = new com.gandalp.gandalp.common.entity.QBaseEntity(this);
@@ -26,6 +29,8 @@ public class QRoom extends EntityPathBase<Room> {
 
     //inherited
     public final StringPath createdBy = _super.createdBy;
+
+    public final QHospital hospital;
 
     public final NumberPath<Long> id = createNumber("id", Long.class);
 
@@ -38,15 +43,24 @@ public class QRoom extends EntityPathBase<Room> {
     public final StringPath updatedBy = _super.updatedBy;
 
     public QRoom(String variable) {
-        super(Room.class, forVariable(variable));
+        this(Room.class, forVariable(variable), INITS);
     }
 
     public QRoom(Path<? extends Room> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QRoom(PathMetadata metadata) {
-        super(Room.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QRoom(PathMetadata metadata, PathInits inits) {
+        this(Room.class, metadata, inits);
+    }
+
+    public QRoom(Class<? extends Room> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.hospital = inits.isInitialized("hospital") ? new QHospital(forProperty("hospital")) : null;
     }
 
 }
